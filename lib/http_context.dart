bfde011b63a6191f0dbae755ad223dc415b1f128
alenkart rodriguez <Alenkart.rodriguez@claro.com.do>
2018-12-28 09:49:08 -0400

    Now the content is enconded before to be werite in the response

--- lib/http_context.dart
+++ lib/http_context.dart
@@ -2,15 +2,15 @@ import 'dart:io';
 import 'dart:convert';
 
 class HttpContext {
-  Uri uri;
   HttpRequest _request;
   HttpResponse _response;
 
   HttpContext(HttpRequest request) {
     this._request = request;
     this._response = request.response;
-    this.uri = this._request.uri;
   }
+
+  get uri => this._request.uri;
 
   get body async {
     String content = await this._request.transform(utf8.decoder).join();
@@ -36,9 +36,12 @@ class HttpContext {
   }
 
   json(Map content) {
+
+    final json = jsonEncode(content);
+
     this._response
       ..headers.set('Content-Type', 'application/json')
-      ..write(content)
+      ..write(json)
       ..close();
   }
 }
